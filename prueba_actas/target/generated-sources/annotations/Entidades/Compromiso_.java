46b0bf386e7d32c3e95b715cdbd852c592634890
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2018-06-27 16:38:02 -0500

    Implementacion de iconos FontAwesome

--- prueba_actas/target/generated-sources/annotations/Entidades/Compromiso_.java
+++ prueba_actas/target/generated-sources/annotations/Entidades/Compromiso_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-27T15:42:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-27T16:35:28")
 @StaticMetamodel(Compromiso.class)
 public class Compromiso_ { 
 
